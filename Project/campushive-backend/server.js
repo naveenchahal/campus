8b24959ad24752dbe08905bfd4fd85e1a30d3364
naveenchahal <n6179323@gmail.com>
2026-05-01 17:01:43 +0530

    asd

--- Project/campushive-backend/server.js
+++ Project/campushive-backend/server.js
@@ -22,7 +22,7 @@ const httpServer = http.createServer(app);
 // ✅ Allowed origins
 const allowedOrigins = [
   "http://localhost:5173",
-  "campushive02y.netlify.app",
+  "https://campushive02y.netlify.app",  
 ];
 
 // ✅ CORS OPTIONS (dynamic)
